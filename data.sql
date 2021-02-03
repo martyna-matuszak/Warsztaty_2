4e0697ba9f55bf4bde9ebe849ca3cc95233d4b55
Martyna <martyna.matuszak96@gmail.com>
2021-02-03 12:27:23 +0100

    Data.sql file organized

--- data.sql
+++ data.sql
@@ -41,35 +41,27 @@ INSERT INTO `exercises` (`id`, `title`, `description`) VALUES (1,'Zadanie 1','St
 UNLOCK TABLES;
 
 --
--- Table structure for table `solutions`
+-- Table structure for table `users_groups`
 --
 
-DROP TABLE IF EXISTS `solutions`;
+DROP TABLE IF EXISTS `users_groups`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `solutions` (
+CREATE TABLE `users_groups` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `created` datetime DEFAULT NULL,
-  `updated` datetime DEFAULT NULL,
-  `description` text COLLATE utf8mb4_unicode_ci,
-  `exercise_id` int(11) DEFAULT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `exercise_id` (`exercise_id`),
-  KEY `user_id` (`user_id`),
-  CONSTRAINT `solutions_ibfk_1` FOREIGN KEY (`exercise_id`) REFERENCES `exercises` (`id`),
-  CONSTRAINT `solutions_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `solutions`
+-- Dumping data for table `users_groups`
 --
 
-LOCK TABLES `solutions` WRITE;
-/*!40000 ALTER TABLE `solutions` DISABLE KEYS */;
-INSERT INTO `solutions` (`id`, `created`, `updated`, `description`, `exercise_id`, `user_id`) VALUES (1,'2020-03-08 14:55:36','2020-02-16 19:56:48','Utworzono klasę, usunięto błędy',1,1),(2,'2020-01-12 13:20:15','2020-02-25 19:56:48','Zadanie rozwiązane',1,2),(3,'2020-01-05 12:50:15','2020-02-14 19:56:48','Zadanie wykonane',2,2),(4,'2020-01-14 12:12:15','2020-02-18 19:56:48','Połączono',3,3),(5,'2020-01-30 12:02:15','2020-02-01 19:56:48','Połączono i rozwiązano problemy',3,4),(7,'2020-03-08 15:04:46',NULL,NULL,3,7),(8,'2020-03-11 11:49:17',NULL,NULL,3,4),(9,NULL,'2020-03-11 16:36:53','Rozwiązane poprawnie',2,1);
-/*!40000 ALTER TABLE `solutions` ENABLE KEYS */;
+LOCK TABLES `users_groups` WRITE;
+/*!40000 ALTER TABLE `users_groups` DISABLE KEYS */;
+INSERT INTO `users_groups` (`id`, `name`) VALUES (1,'java'),(3,'python'),(4,'testerzy'),(5,'frontend');
+/*!40000 ALTER TABLE `users_groups` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -98,33 +90,43 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` (`id`, `email`, `username`, `password`, `user_group_id`) VALUES (1,'martyna@onet.pl','martyna','$2a$10$PiV5YuO.K7u4xbblO1jyyuE09tOdwwnNJ/G6LgCyH8WhOB5TM9uQ2',1),(2,'katarzyna@wp.pl','kasia','$2a$10$OLTG.f0WDwa9IXrYEm8qBeCWbEvsEu9ho8SdchXcCi5I.mGXcPFY.',1),(3,'marek@gmail.com','marek','$2a$10$3fSz8iZJEzrKchiKM6EeQOoQ94l9r4bR7fWwvycOnnFjWvdMjGUnG',3),(4,'jaroslaw@wp.pl','jarek','$2a$10$ZKbTF/QSnIZaBb2K.fSf8OvzxUByKWO0BcNVnvV71vNaPhdpqvvn.',3),(6,'krzysztofG@poczta.onet.pl','krzysiuG','krzysiuG',3),(7,'barbaranowacka@gmail.com','nowackabbb','nowackabbb',3),(8,'alicjawojak@op.pl','alaala','alaala',3);
+INSERT INTO `users` (`id`, `email`, `username`, `password`, `user_group_id`) VALUES (1,'martyna@onet.pl','martyna','$2a$10$PiV5YuO.K7u4xbblO1jyyuE09tOdwwnNJ/G6LgCyH8WhOB5TM9uQ2',1),(2,'katarzyna@wp.pl','kasia','$2a$10$OLTG.f0WDwa9IXrYEm8qBeCWbEvsEu9ho8SdchXcCi5I.mGXcPFY.',1),(3,'marek@gmail.com','marek','$2a$10$3fSz8iZJEzrKchiKM6EeQOoQ94l9r4bR7fWwvycOnnFjWvdMjGUnG',3),(4,'jaroslaw@wp.pl','jarek','$2a$10$ZKbTF/QSnIZaBb2K.fSf8OvzxUByKWO0BcNVnvV71vNaPhdpqvvn.',3);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `users_groups`
+-- Table structure for table `solutions`
 --
 
-DROP TABLE IF EXISTS `users_groups`;
+DROP TABLE IF EXISTS `solutions`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `users_groups` (
+CREATE TABLE `solutions` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `created` datetime DEFAULT NULL,
+  `updated` datetime DEFAULT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci,
+  `exercise_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `exercise_id` (`exercise_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `solutions_ibfk_1` FOREIGN KEY (`exercise_id`) REFERENCES `exercises` (`id`),
+  CONSTRAINT `solutions_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `users_groups`
+-- Dumping data for table `solutions`
 --
 
-LOCK TABLES `users_groups` WRITE;
-/*!40000 ALTER TABLE `users_groups` DISABLE KEYS */;
-INSERT INTO `users_groups` (`id`, `name`) VALUES (1,'java'),(3,'python'),(4,'testerzy'),(5,'frontend');
-/*!40000 ALTER TABLE `users_groups` ENABLE KEYS */;
+LOCK TABLES `solutions` WRITE;
+/*!40000 ALTER TABLE `solutions` DISABLE KEYS */;
+INSERT INTO `solutions` (`id`, `created`, `updated`, `description`, `exercise_id`, `user_id`) VALUES (1,'2020-03-08 14:55:36','2020-02-16 19:56:48','Utworzono klasę, usunięto błędy',1,1),(2,'2020-01-12 13:20:15','2020-02-25 19:56:48','Zadanie rozwiązane',1,2),(3,'2020-01-05 12:50:15','2020-02-14 19:56:48','Zadanie wykonane',2,2),(4,'2020-01-14 12:12:15','2020-02-18 19:56:48','Połączono',3,3),(5,'2020-01-30 12:02:15','2020-02-01 19:56:48','Połączono i rozwiązano problemy',3,4),(7,'2020-03-08 15:04:46',NULL,NULL,3,7),(8,'2020-03-11 11:49:17',NULL,NULL,3,4);
+/*!40000 ALTER TABLE `solutions` ENABLE KEYS */;
 UNLOCK TABLES;
+
+
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
